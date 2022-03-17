d2100ece5ed0e2263c9a69fd7bd24d4f58f217f0
DeepDileep <deepdileep@gmail.com>
2022-03-17 16:30:07 +0530

    First Function added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const h1 = document.querySelector('h1')
+const body = document.querySelector('body')
+const button = document.querySelector('button')
+const link = document.querySelector('a')
+
+button.addEventListener('click', function(){
+
+    h1.textContent = "You saved me"
+
+    body.style.backgroundColor = "orange"
+
+    link.textContent = "🎆🎆🎆"
+})
